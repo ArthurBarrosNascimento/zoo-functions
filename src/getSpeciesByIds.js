748924f1dbd58d2341ba6e7940edba10e6a85e00
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-07-07 23:37:14 -0300

    Requisito um aprovado nos testes

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
-
-function getSpeciesByIds(ids) {
+const { species, employees, hours, prices } = require('../data/zoo_data');
+function getSpeciesByIds(...idsArray) {
   // seu código aqui
-}
+  const result = [];
 
+  idsArray.forEach((id) => {
+    const specie = species.find((specie) => specie.id == id);
+    result.push(specie);
+  })
+
+  return result;
+}
+getSpeciesByIds();
 module.exports = getSpeciesByIds;
