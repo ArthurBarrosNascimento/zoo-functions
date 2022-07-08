f3d988f51ce7f70cea2b671a96a60fc9fdc4f7cc
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-07-08 17:43:44 -0300

    terceiro requisito feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const employe = employees.find(({ firstName, lastName }) => (
+    firstName === employeeName || lastName === employeeName));
+  console.log(employe);
+
+  const emptyResult = {};
+
+  if (employe === undefined) return emptyResult;
+
+  return employe;
 }
 
 module.exports = getEmployeeByName;
