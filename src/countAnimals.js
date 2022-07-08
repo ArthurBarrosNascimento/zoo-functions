7d125624bde7467d594ffd7b9fbcac5b9ad5f6e8
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-07-08 19:22:56 -0300

    quinto requisito feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,38 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+function defaultAnimalsCounter() {
+  const result = {};
+
+  species.forEach((specie) => {
+    result[specie.name] = specie.residents.length;
+  });
+
+  return result;
+}
+
+function animalsCounterBySpecie(specie) {
+  const animal = species.find(({ name }) => name === specie);
+
+  return animal.residents.length;
+}
+
+function animalsCouterBySpecieAndSex(specie, animalSex) {
+  const animal = species.find(({ name }) => name === specie);
+
+  const quantity = animal.residents.filter(({ sex }) => sex === animalSex).length;
+
+  return quantity;
+}
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) return defaultAnimalsCounter();
+
+  const { specie, sex } = animal;
+
+  if (specie && sex) return animalsCouterBySpecieAndSex(specie, sex);
+
+  if (specie) return animalsCounterBySpecie(specie);
 }
 
 module.exports = countAnimals;
