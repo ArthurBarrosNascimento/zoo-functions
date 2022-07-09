cdc330b004073c1802475c8aa6c4e7f98ede5302
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-07-09 17:46:37 -0300

    requisito oito feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  // seu código aqu
+  const result = {};
+
+  const childs = entrants.filter(({ age }) => age < 18).length;
+
+  const adults = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+
+  const olds = entrants.filter(({ age }) => age >= 50).length;
+
+  result.child = childs;
+  result.adult = adults;
+  result.senior = olds;
+
+  return result;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0 || entrants.length === 0) {
+    return 0;
+  }
+
+  const { child, adult, senior } = countEntrants(entrants);
+
+  const allTicketPrices = (
+    (prices.child * child) + (prices.adult * adult) + (prices.senior * senior));
+  return allTicketPrices;
 }
 
 module.exports = { calculateEntry, countEntrants };
