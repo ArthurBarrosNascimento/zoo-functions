05480599ebf241731982ff89bb12f3f283590c5f
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-07-10 15:27:51 -0300

    RRequisito 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
+function getOldestFromFirstSpecies(idEmployee) {
   // seu código aqui
+  const employee = employees.find(({ id }) => id === idEmployee);
+
+  const specieId = employee.responsibleFor.find((responsibleFor) => responsibleFor.length);
+
+  const animalId = species.find((animal) => animal.id === specieId);
+
+  const ages = animalId.residents.map(({ age }) => Math.max(age));
+  console.log(ages);
+  const animlaOlder = ages.indexOf(Math.max(...ages));
+
+  return Object.values(animalId.residents[animlaOlder]);
 }
 
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getOldestFromFirstSpecies;
