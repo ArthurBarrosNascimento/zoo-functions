c054f49925178a75d590d0925f50753d2dac4338
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-07-11 00:36:25 -0300

    requisito 14 feito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,59 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const locationsList = [
+  'NE',
+  'NW',
+  'SE',
+  'SW',
+];
+
+function getSpecies(locationName) {
+  return species.filter(({ location }) => location === locationName);
+}
+
+function getSpeciesByLocation(locationList) {
+  const result = {};
+
+  locationList.forEach((locationName) => {
+    const filteredSpecies = getSpecies(locationName);
+    result[locationName] = filteredSpecies.map(({ name }) => name);
+  });
+
+  return result;
+}
+
+function sortNames(namesList, isSorted) {
+  if (isSorted) return namesList.sort();
+
+  return namesList;
+}
+
+function getSpeciesByLocationWithName(locationList, sexo, isSorted) {
+  const result = {};
+
+  locationList.forEach((locationName) => {
+    result[locationName] = [];
+
+    const filteredSpecies = getSpecies(locationName);
+
+    filteredSpecies.forEach(({ name, residents }) => {
+      const filteredBySex = sexo ? residents.filter(({ sex }) => sex === sexo) : residents;
+
+      result[locationName].push({
+        [name]: sortNames(filteredBySex.map((resident) => resident.name), isSorted),
+      });
+    });
+  });
+
+  return result;
+}
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options || !options.includeNames) return getSpeciesByLocation(locationsList);
+  const { includeNames, sex, sorted } = options;
+
+  if (includeNames) return getSpeciesByLocationWithName(locationsList, sex, sorted);
 }
 
 module.exports = getAnimalMap;
