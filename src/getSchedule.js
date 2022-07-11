1bc3f266e79282430523c1a3c2d853b180a65b95
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-07-10 22:39:42 -0300

    Requisito nove feito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,75 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+
+const weekdays = [
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+  'Monday',
+];
+
+function getAnimalScheduleByName(animalName) {
+  const animal = species.find(({ name }) => name === animalName);
+
+  return animal ? animal.availability : undefined;
+}
+
+function getAnimalByWeekDay(day) {
+  if (day === 'Monday') return 'The zoo will be closed!';
+
+  const result = [];
+
+  species.forEach(({ availability, name }) => {
+    if (availability.includes(day)) {
+      result.push(name);
+    }
+  });
+
+  return result;
+}
+
+function hoursAlert(day) {
+  let result = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+
+  if (day === 'Monday') {
+    result = 'CLOSED';
+  }
+
+  return result;
+}
+
+function getExibitionDays() {
+  const result = {};
+
+  weekdays.forEach((day) => {
+    result[day] = {
+      officeHour: hoursAlert(day),
+      exhibition: getAnimalByWeekDay(day),
+    };
+  });
+
+  return result;
+}
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  const animalSchedule = getAnimalScheduleByName(scheduleTarget);
+
+  if (animalSchedule) return animalSchedule;
+
+  const exhibitionDays = getExibitionDays();
+
+  const weekday = exhibitionDays[scheduleTarget];
+
+  if (weekday === undefined) return exhibitionDays;
+
+  const result = {
+    [scheduleTarget]: exhibitionDays[scheduleTarget],
+  };
+
+  return result;
 }
 
 module.exports = getSchedule;
